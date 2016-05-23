3b7965ac4a0bc9b67f743c08383965a9add442e5
Rex Tsai (蔡志展) <rex.cc.tsai@gmail.com>
2016-05-24 01:52:37 +0800

    Set user and db name environment variables.

--- postgis.sh
+++ postgis.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-POSTGIS_INSTANCE=${1:-"osm-postgis"}
-POSTGIS_PASSWORD=${2:-"mysecretpassword"}
-docker run --name ${POSTGIS_INSTANCE} -e POSTGRES_PASSWORD=${POSTGIS_PASSWORD} -d osmtw/postgis
+POSTGRES_USER=postgres
+POSTGRES_DB=postgres
+POSTGIS_INSTANCE=${1:-"osmdb"}
+POSTGIS_PASSWORD=${2:-$(cat /dev/urandom| tr -dc _A-Z-a-z-0-9 | head -c12)}
+docker run --name ${POSTGIS_INSTANCE} \
+    -e POSTGRES_PASSWORD=${POSTGIS_PASSWORD} \
+    -e POSTGRES_USER=${POSTGRES_USER} \
+    -e POSTGRES_DB=${POSTGRES_DB} \
+    -d osmtw/postgis
